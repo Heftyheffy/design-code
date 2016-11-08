9316968d5b016b63e74347e5d229c5c4f73b0791
Jeff <jeffrey.kim.0122@gmail.com>
2016-11-08 15:57:48 -0500

    before messing with scroll pic

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,30 @@
-$(function() {
-  //designRubberBand();
+/*
+$(document).ready(function() {
+
   setInterval(function(){designRubberBand()}, 4000);
+
+});
+*/
+
+$(window).scroll(function() {
+  notesScroll();
+
 });
 
+
+function notesScroll() {
+  var wScroll = $(window).scrollTop();
+
+  $('.picture-strip').css('background', 'center ' + wScroll + 'px');
+
+}
+
+
+
 function designRubberBand(){
+
   var randomNumber = Math.floor(Math.random() * $('.article-thumb').length) + 1
+
   $('.article-thumb').eq(randomNumber).addClass('is-emph')
     .siblings().removeClass('is-emph');
 }
