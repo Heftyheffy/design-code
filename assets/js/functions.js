bf7018634afb73a660da5bc608b66908b79564b8
Jeff <jeffrey.kim.0122@gmail.com>
2016-11-09 00:53:40 -0500

    beginning on footer

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,11 +1,30 @@
-/*
 $(document).ready(function() {
 
   setInterval(function(){designRubberBand()}, 4000);
 
-});
-*/
+  hoverNoteimgpic();
 
+});
+
+function hoverNoteimgpic() {
+  $('.notes-img-pic').hover(function(){
+    //hover over picture-strip, then fade out
+    $(this).fadeOut(700).finish(600);
+  }, function(){
+    $(this).fadeIn(700);
+  });
+
+}
+
+function designRubberBand(){
+
+  var randomNumber = Math.floor(Math.random() * $('.article-thumb').length) + 1
+
+  $('.article-thumb').eq(randomNumber).addClass('is-emph')
+    .siblings().removeClass('is-emph');
+}
+
+/*
 $(window).scroll(function() {
   notesScroll();
 
@@ -18,13 +37,4 @@ function notesScroll() {
   $('.picture-strip').css('background', 'center ' + wScroll + 'px');
 
 }
-
-
-
-function designRubberBand(){
-
-  var randomNumber = Math.floor(Math.random() * $('.article-thumb').length) + 1
-
-  $('.article-thumb').eq(randomNumber).addClass('is-emph')
-    .siblings().removeClass('is-emph');
-}
+*/
